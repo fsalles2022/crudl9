a822c0c7a1910a8b87263e4913e5e4c7d40246df
fsalles2022 <fabiolfs76@hotmail.com>
2023-01-07 18:38:33 -0300

    Adicionado função destroy na HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -70,6 +70,11 @@ class HomeController extends Controller
     public function destroy($id)
     {
         // softDelete
-        
+        //dd($id);
+        $user = User::find($id);
+        $user->delete();
+        return redirect()->back();
+
+
     }
 }
